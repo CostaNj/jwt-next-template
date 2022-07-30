9596b3c376190ca076a1895bdd9256a89a445740
CostaNj <costa.n.work@yandex.ru>
2022-07-31 00:47:31 +0300

    change useQuery to useMutation (registration + login)

--- client/containers/login-form.tsx
+++ client/containers/login-form.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useCallback, useState } from 'react'
 import { AxiosError, AxiosResponse } from 'axios'
-import { useQuery } from 'react-query'
+import { useMutation } from 'react-query'
 import { useRouter } from 'next/router'
 import LoadingButton from '@mui/lab/LoadingButton'
 import { Box, TextField } from '@mui/material'
@@ -17,7 +17,7 @@ export const LoginForm = () => {
   const [password, setPassword] = useState<string>('test')
   const [passwordError, setPasswordError] = useState<string>('')
 
-  const { refetch, isLoading } = useQuery<AxiosResponse, AxiosError>(
+  const { mutate: registrationMutate, isLoading: registrationLoading } = useMutation<AxiosResponse, AxiosError>(
     'registration',
     async () => await registration(email, password),
     {
@@ -38,7 +38,7 @@ export const LoginForm = () => {
     }
   )
 
-  const { refetch: loginRefetch, isLoading: loginLoading } = useQuery(
+  const { mutate: loginMutate, isLoading: loginLoading } = useMutation(
     'login',
     async () => await login(email, password),
     {
@@ -55,12 +55,12 @@ export const LoginForm = () => {
   )
 
   const handleClickRegistration = useCallback(() => {
-    refetch()
-  }, [email, password, isLoading, refetch])
+    registrationMutate()
+  }, [email, password, registrationLoading])
 
   const handleClickLogin = useCallback(() => {
-    loginRefetch()
-  }, [email, password, loginLoading, loginRefetch])
+    loginMutate()
+  }, [email, password, loginLoading])
 
   const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value)
@@ -132,7 +132,7 @@ export const LoginForm = () => {
         <LoadingButton
           size="small"
           onClick={handleClickRegistration}
-          loading={isLoading}
+          loading={registrationLoading}
           loadingIndicator="Loading…"
           variant="outlined"
           sx={{ height: '30px' }}
